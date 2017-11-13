79d155c8f9833978a82e3093a1a3afde7337939e
brian-swan <bswan@microsoft.com>
2017-11-13 10:47:28 -0800

    added ARM templates and PS script

---
+++ ConfigureWebSrvAndDeployAzTodo.ps1
@@ -0,0 +1,142 @@
+﻿# This script configures a Windows 2016 Datacenter VM to run AzTodo:
+# 1. Enables IIS with necessary features
+# 2. Installs ASP.NET Core
+# 4. Deploys AzTodo
+# 5. Opens ports 80
+param(
+
+    [Parameter(Mandatory=$True)]
+    [string]$ServerEndpoint,
+
+    [Parameter(Mandatory=$False)]
+    [string]$SqlUserName = "aztodosqluser",
+
+    [Parameter(Mandatory=$True)]
+    [string]$SqlPwd
+)
+
+function Create-ASPStateDB
+{
+    try
+    {
+        $aspNetRegSqlPath = "C:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_regsql.exe"
+        if(-not (Test-Path -Path $aspNetRegSqlPath))
+        {
+            throw "aspnet_regsql.exe not found."
+        }
+        Start-Process -FilePath $aspNetRegSqlPath -ArgumentList "-S $ServerEndpoint -U $SqlUserName -P $SqlPwd -ssadd -sstype p"
+        
+        Write-Host "Created ASPState DB successfully."
+    }
+    catch [Exception]
+    {
+        Write-Host "Creating ASPState DB failed with exception:" $_.Exception.GetType().FullName
+        Write-Host $_.Exception.Message
+        exit 1
+    }
+}
+
+function Install-AspNetCore
+{
+	$webClient = New-Object -TypeName System.Net.WebClient
+	$destination= "C:\WindowsAzure\DotNetCore.2.0.0-WindowsHosting.exe"
+	$webClient.DownloadFile("https://aka.ms/dotnetcore.2.0.0-windowshosting",$destination)
+	Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force
+	Start-Process -FilePath "C:\WindowsAzure\DotNetCore.2.0.0-WindowsHosting.exe" -ArgumentList "/quiet" -Wait
+	Invoke-Command -scriptblock {iisreset}
+}
+
+function Deploy-AzTodo
+{
+    try
+    {
+        Add-Type -AssemblyName System.IO.Compression.FileSystem
+
+        # Get zip file and unpack
+        $blobName = "AzTodo.zip"
+        $storageAccountName = "azurestackbetastorage"
+        $container = "aztodo"
+        $outpath = "$env:temp\AzTodo"
+        $context = New-AzureStorageContext -StorageAccountName $storageAccountName -Anonymous
+        Get-AzureStorageBlobContent -Context $context -Container $container -Blob $blobName -Destination $env:temp -Force
+        [System.IO.Compression.ZipFile]::ExtractToDirectory("$env:temp\$blobName", $outpath)
+
+        $defaultConnString = "Server=tcp:$ServerEndpoint,1433;Integrated Security=false;User ID=$SqlUserName;Password=$SqlPwd;Database=AzTodo" 
+        #$sessionConnString = "Server=tcp:$ServerEndpoint,1433;Integrated Security=false;User ID=$SqlUserName;Password=$SqlPwd;Database=ASPState"
+        $appSettingsJson = Get-Content $outpath\appsettings.json -Raw | ConvertFrom-Json
+        $appSettingsJson.ConnectionStrings.DefaultConnection = $defaultConnString
+        $appSettingsJson | ConvertTo-Json | Set-Content $outpath\appsettings.json
+
+        # Copy contents to C:\inetpub\wwwroot
+        Copy-Item $env:temp\AzTodo\* C:\inetpub\wwwroot -Recurse -Force
+        
+        Write-Host "Deployed AzTodo successfully."
+    }
+        catch [Exception]
+    {
+        Write-Host "Deploying AzTodo failed with exception:" $_.Exception.GetType().FullName
+        Write-Host $_.Exception.Message
+        exit 1
+    }
+}
+
+# AzureRM module functions are needed for accessing blob storage.
+try
+{ 
+    $azureRmModule = Get-Module | Where-Object { $_.Name -eq "AzureRM" }
+    if($azureRmModule -eq $null)
+    {
+        Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+        Install-Module AzureRM -Force
+        Import-Module AzureRM
+    }
+
+    Write-Host "AzureRM module installed successfully."
+}
+catch [Exception]
+{
+    Write-Host "Installing AzureRM module failed with exception:" $_.Exception.GetType().FullName
+    Write-Host $_.Exception.Message
+    exit 1
+}
+
+try
+{
+    # Enable IIS-WebServerRole and ASPNET45
+    if((Get-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole).State -eq "Disabled")
+    {
+        Enable-WindowsOptionalFeature -Online -FeatureName IIS-WebServerRole -All
+    }
+
+    <#if((Get-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45).State -eq "Disabled")
+    {
+        Enable-WindowsOptionalFeature -Online -FeatureName IIS-ASPNET45 -All
+    }#>
+    
+    Write-Host "IIS enabled successfully."
+}
+catch [Exception]
+{
+    Write-Host "Enabling IIS features failed with exception:" $_.Exception.GetType().FullName
+    Write-Host $_.Exception.Message
+    exit 1
+}
+
+# Finish AzTodo prerequisites and deploy AzTodo
+#Create-ASPStateDB
+Install-AspNetCore
+Deploy-AzTodo
+
+try
+{
+    # Open ports 80
+    netsh advfirewall firewall add rule name="Open port 80" dir=in action=allow protocol=TCP localport=80
+    
+    Write-Host "Ports 80 opened successfully."
+}
+catch [Exception]
+{
+    Write-Host "Open portsfailed with exception:" $_.Exception.GetType().FullName
+    Write-Host $_.Exception.Message
+    exit 1
+}
---
+++ CreateAzTodoDB.sql
@@ -0,0 +1,284 @@
+CREATE DATABASE AzTodo
+GO
+
+USE AzTodo
+GO
+
+IF OBJECT_ID(N'__EFMigrationsHistory') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetRoles] (
+        [Id] nvarchar(450) NOT NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        [Name] nvarchar(256) NULL,
+        [NormalizedName] nvarchar(256) NULL,
+        CONSTRAINT [PK_AspNetRoles] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetUserTokens] (
+        [UserId] nvarchar(450) NOT NULL,
+        [LoginProvider] nvarchar(450) NOT NULL,
+        [Name] nvarchar(450) NOT NULL,
+        [Value] nvarchar(max) NULL,
+        CONSTRAINT [PK_AspNetUserTokens] PRIMARY KEY ([UserId], [LoginProvider], [Name])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetUsers] (
+        [Id] nvarchar(450) NOT NULL,
+        [AccessFailedCount] int NOT NULL,
+        [ConcurrencyStamp] nvarchar(max) NULL,
+        [Email] nvarchar(256) NULL,
+        [EmailConfirmed] bit NOT NULL,
+        [LockoutEnabled] bit NOT NULL,
+        [LockoutEnd] datetimeoffset NULL,
+        [NormalizedEmail] nvarchar(256) NULL,
+        [NormalizedUserName] nvarchar(256) NULL,
+        [PasswordHash] nvarchar(max) NULL,
+        [PhoneNumber] nvarchar(max) NULL,
+        [PhoneNumberConfirmed] bit NOT NULL,
+        [SecurityStamp] nvarchar(max) NULL,
+        [TwoFactorEnabled] bit NOT NULL,
+        [UserName] nvarchar(256) NULL,
+        CONSTRAINT [PK_AspNetUsers] PRIMARY KEY ([Id])
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetRoleClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        [RoleId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetRoleClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetRoleClaims_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetUserClaims] (
+        [Id] int NOT NULL IDENTITY,
+        [ClaimType] nvarchar(max) NULL,
+        [ClaimValue] nvarchar(max) NULL,
+        [UserId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetUserClaims] PRIMARY KEY ([Id]),
+        CONSTRAINT [FK_AspNetUserClaims_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetUserLogins] (
+        [LoginProvider] nvarchar(450) NOT NULL,
+        [ProviderKey] nvarchar(450) NOT NULL,
+        [ProviderDisplayName] nvarchar(max) NULL,
+        [UserId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetUserLogins] PRIMARY KEY ([LoginProvider], [ProviderKey]),
+        CONSTRAINT [FK_AspNetUserLogins_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE TABLE [AspNetUserRoles] (
+        [UserId] nvarchar(450) NOT NULL,
+        [RoleId] nvarchar(450) NOT NULL,
+        CONSTRAINT [PK_AspNetUserRoles] PRIMARY KEY ([UserId], [RoleId]),
+        CONSTRAINT [FK_AspNetUserRoles_AspNetRoles_RoleId] FOREIGN KEY ([RoleId]) REFERENCES [AspNetRoles] ([Id]) ON DELETE CASCADE,
+        CONSTRAINT [FK_AspNetUserRoles_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [IX_AspNetRoleClaims_RoleId] ON [AspNetRoleClaims] ([RoleId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [IX_AspNetUserClaims_UserId] ON [AspNetUserClaims] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [IX_AspNetUserLogins_UserId] ON [AspNetUserLogins] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [IX_AspNetUserRoles_RoleId] ON [AspNetUserRoles] ([RoleId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [IX_AspNetUserRoles_UserId] ON [AspNetUserRoles] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE INDEX [EmailIndex] ON [AspNetUsers] ([NormalizedEmail]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'00000000000000_CreateIdentitySchema')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'00000000000000_CreateIdentitySchema', N'2.0.0-rtm-26452');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171019230048_InitialCreate')
+BEGIN
+    DROP INDEX [UserNameIndex] ON [AspNetUsers];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171019230048_InitialCreate')
+BEGIN
+    DROP INDEX [IX_AspNetUserRoles_UserId] ON [AspNetUserRoles];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171019230048_InitialCreate')
+BEGIN
+    DROP INDEX [RoleNameIndex] ON [AspNetRoles];
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171019230048_InitialCreate')
+BEGIN
+    CREATE UNIQUE INDEX [UserNameIndex] ON [AspNetUsers] ([NormalizedUserName]) WHERE [NormalizedUserName] IS NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171019230048_InitialCreate')
+BEGIN
+    CREATE UNIQUE INDEX [RoleNameIndex] ON [AspNetRoles] ([NormalizedName]) WHERE [NormalizedName] IS NOT NULL;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171019230048_InitialCreate')
+BEGIN
+    ALTER TABLE [AspNetUserTokens] ADD CONSTRAINT [FK_AspNetUserTokens_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE CASCADE;
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171019230048_InitialCreate')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20171019230048_InitialCreate', N'2.0.0-rtm-26452');
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171020005627_Lists')
+BEGIN
+    CREATE TABLE [Lists] (
+        [TodoListId] int NOT NULL IDENTITY,
+        [TodoListName] nvarchar(max) NULL,
+        [UserId] nvarchar(450) NULL,
+        CONSTRAINT [PK_Lists] PRIMARY KEY ([TodoListId]),
+        CONSTRAINT [FK_Lists_AspNetUsers_UserId] FOREIGN KEY ([UserId]) REFERENCES [AspNetUsers] ([Id]) ON DELETE NO ACTION
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171020005627_Lists')
+BEGIN
+    CREATE TABLE [Items] (
+        [ItemId] int NOT NULL IDENTITY,
+        [ItemDescription] nvarchar(max) NULL,
+        [TodoListId] int NOT NULL,
+        CONSTRAINT [PK_Items] PRIMARY KEY ([ItemId]),
+        CONSTRAINT [FK_Items_Lists_TodoListId] FOREIGN KEY ([TodoListId]) REFERENCES [Lists] ([TodoListId]) ON DELETE CASCADE
+    );
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171020005627_Lists')
+BEGIN
+    CREATE INDEX [IX_Items_TodoListId] ON [Items] ([TodoListId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171020005627_Lists')
+BEGIN
+    CREATE INDEX [IX_Lists_UserId] ON [Lists] ([UserId]);
+END;
+
+GO
+
+IF NOT EXISTS(SELECT * FROM [__EFMigrationsHistory] WHERE [MigrationId] = N'20171020005627_Lists')
+BEGIN
+    INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20171020005627_Lists', N'2.0.0-rtm-26452');
+END;
+
+GO
+
